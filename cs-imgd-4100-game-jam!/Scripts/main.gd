1de91702d2794e6e240147b19bb43769b79aaf2f
sammas4 <sammasciave@gmail.com>
2025-10-04 17:22:47 -0400

    Global Vars again oh my god

--- cs-imgd-4100-game-jam!/Scripts/main.gd
+++ cs-imgd-4100-game-jam!/Scripts/main.gd
@@ -3,7 +3,7 @@ extends Node2D
 @onready var timer = $Timer
 @onready var time = $Time
 @onready var scorePrint = $ScoreCt
-@onready var score = GameManager.current_score
+@onready var score = GlobalVars.current_score
 
 func _process(delta: float) -> void:
 	time.set_text(str(snapped(timer.get_time_left(), 0.01)))
