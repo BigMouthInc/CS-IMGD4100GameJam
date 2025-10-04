0c88edfc9494e7485ed876c25f3775f57df69c2f
sammas4 <sammasciave@gmail.com>
2025-10-04 17:18:19 -0400

    Added global vars and unfucked shit

--- cs-imgd-4100-game-jam!/Scripts/game_manager.gd
+++ cs-imgd-4100-game-jam!/Scripts/game_manager.gd
@@ -1,6 +1,5 @@
 extends Node
 
-var current_score = 0
 @onready var block = $"../Spawner/Block"
 
 func _ready() -> void:
@@ -8,5 +7,5 @@ func _ready() -> void:
 
 	
 func add_score(amount: int):
-	current_score += amount
-	print("Score: ", current_score)
+	GlobalVars.current_score += amount
+	print("Score: ", GlobalVars.current_score)
--- cs-imgd-4100-game-jam!/Scripts/main.gd
+++ cs-imgd-4100-game-jam!/Scripts/main.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 @onready var timer = $Timer
 @onready var time = $Time
-
+@onready var scorePrint = $ScoreCt
 
 func _process(delta: float) -> void:
 	time.set_text(str(snapped(timer.get_time_left(), 0.01)))
+	scorePrint.set_text("Score: " + str(GlobalVars.current_score))
