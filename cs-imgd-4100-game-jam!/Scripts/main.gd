5bb94a1237a63a6cbbe30cae08039902321ac174
sammas4 <sammasciave@gmail.com>
2025-10-04 17:06:23 -0400

    Added score label but game buggin :(

--- cs-imgd-4100-game-jam!/Scripts/main.gd
+++ cs-imgd-4100-game-jam!/Scripts/main.gd
@@ -2,7 +2,9 @@ extends Node2D
 
 @onready var timer = $Timer
 @onready var time = $Time
-
+@onready var scorePrint = $ScoreCt
+@onready var score = GameManager.current_score
 
 func _process(delta: float) -> void:
 	time.set_text(str(snapped(timer.get_time_left(), 0.01)))
+	scorePrint.set_text("Score: " + str(score))
