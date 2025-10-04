088e9dde50638ddd8267eff668f8f82ee96cf9a6
BigMouthInc <144144777+BigMouthInc@users.noreply.github.com>
2025-10-04 17:42:02 -0400

    PLEASE DONT CAUSE ERRORS

--- cs-imgd-4100-game-jam!/Scripts/spawner.gd
+++ cs-imgd-4100-game-jam!/Scripts/spawner.gd
@@ -2,12 +2,15 @@ extends Node2D
 
 @onready var block = preload("res://Scenes/block.tscn")
 
-func _on_timer_timeout() -> void:
-	var viewport_size = get_viewport_rect().size
-	var random_x = randf_range(0, viewport_size.x)
-	var random_y = randf_range(0, viewport_size.y)
+func _on_block_timer_timeout() -> void:
+	
+	var random_x = randf_range(0, 550)
+	var random_y = randf_range(-330, 0)
 	var random_position = Vector2(random_x, random_y)
 	
 	var placeBlock  = block.instantiate()
 	placeBlock.position = random_position
 	get_parent().add_child(placeBlock)
+	
+	var game_manager = get_node("/root/Main/GameManager")
+	placeBlock.point_signal.connect(game_manager.add_score)
