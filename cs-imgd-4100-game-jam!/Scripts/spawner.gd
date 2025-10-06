905346890cdd0ea6406fc72130d54e075280407f
BigMouthInc <144144777+BigMouthInc@users.noreply.github.com>
2025-10-05 20:47:08 -0400

    shoudlnt be as close

--- cs-imgd-4100-game-jam!/Scripts/spawner.gd
+++ cs-imgd-4100-game-jam!/Scripts/spawner.gd
@@ -7,12 +7,12 @@ var positions = []
 func _on_block_timer_timeout() -> void:
 	
 	var random_x = randf_range(50, 550)
-	var random_y = randf_range(-330, -50)
+	var random_y = randf_range(-330, -60)
 	var random_position = Vector2(random_x, random_y)
 	
 	while nearBlock(random_position):
 		random_x = randf_range(50, 550)
-		random_y = randf_range(-310, -50)
+		random_y = randf_range(-310, -60)
 		random_position = Vector2(random_x, random_y)
 	
 	positions.append(random_position)
@@ -27,9 +27,9 @@ func _on_block_timer_timeout() -> void:
 func nearBlock(new_pos: Vector2) -> bool:
 
 	for platform in platforms.get_children():
-		if platform.position.x - 8 < new_pos.x and new_pos.x < platform.position.x + 8:
+		if platform.position.x - 20 < new_pos.x and new_pos.x < platform.position.x + 20:
 			return true
-		if  platform.position.y - 8 > new_pos.y and new_pos.y > platform.position.y + 8:
+		if  platform.position.y - 20 > new_pos.y and new_pos.y > platform.position.y + 20:
 			return true
 			
 	for position in positions:
