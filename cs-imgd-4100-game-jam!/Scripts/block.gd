afbe737481ae63d054cf03d1ff5b35dfbe752d4f
sammas4 <sammasciave@gmail.com>
2025-10-10 14:48:32 -0400

    Dream of animationcationnn

--- cs-imgd-4100-game-jam!/Scripts/block.gd
+++ cs-imgd-4100-game-jam!/Scripts/block.gd
@@ -4,7 +4,9 @@ signal point_signal(score)
 @onready var coin = $Coin
 
 func _on_body_entered(body: Node2D) -> void:
+	$AnimatedSprite2D.play("broked")
 	coin.play()
 	await coin.finished
+	$AnimatedSprite2D.play("idle")
 	queue_free()
 	emit_signal('point_signal', 1)
--- cs-imgd-4100-game-jam!/Scripts/player.gd
+++ cs-imgd-4100-game-jam!/Scripts/player.gd
@@ -4,7 +4,6 @@ extends CharacterBody2D
 const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 @onready var jump = $Jump
-@onready var coin = $Coin
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
